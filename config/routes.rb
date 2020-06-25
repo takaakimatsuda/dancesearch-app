69338d843bb955235ac161bb1be8b6041e065b30
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-06-25 22:40:53 +0900

    ホーム画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ログインしていない時の初期画面
+  root 'home#index'
 end

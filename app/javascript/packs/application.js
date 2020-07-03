3badc9935a62acdf98751475c55d067f4177e80f
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-07-03 10:07:48 +0900

    CSSの設定
    Signed-off-by: 松田　剛明 <takaaki-matsuda@hotmail.co.jp>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")

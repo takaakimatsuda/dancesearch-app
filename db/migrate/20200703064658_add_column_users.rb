fa6e4fa19f69fbb279693d5a057511d6ded3a1e0
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-07-05 11:08:11 +0900

    Userテーブルにカラムを追加

--- db/migrate/20200703064658_add_column_users.rb
+++ db/migrate/20200703064658_add_column_users.rb
@@ -1,4 +1,14 @@
 class AddColumnUsers < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :genre, :string
+    add_column :users, :team_name, :string
+    add_column :users, :pref1, :string
+    add_column :users, :pref2, :string
+    add_column :users, :pref3, :string
+    add_column :users, :carrer, :text
+    add_column :users, :lesson_comment, :text
+    add_column :users, :movie_url, :string
+    add_column :users, :promotion_url, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_071407) do
+ActiveRecord::Schema.define(version: 2020_07_03_064658) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2020_06_22_071407) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "genre"
+    t.string "team_name"
+    t.string "pref1"
+    t.string "pref2"
+    t.string "pref3"
+    t.text "carrer"
+    t.text "lesson_comment"
+    t.string "movie_url"
+    t.string "promotion_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

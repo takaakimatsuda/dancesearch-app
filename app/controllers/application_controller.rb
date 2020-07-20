e0fd420b5d894540d80c8c64c2175538c53aa43b
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-07-20 14:48:46 +0900

    プロフィールのフォームの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,39 @@ class ApplicationController < ActionController::Base
   # deviseコントローラーにストロングパラメータを追加する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  
+  # ログイン後の遷移先を変更
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
+  protected
+
+  # サインアップ時にnameとavatarのストロングパラメータを追加
     def configure_permitted_parameters
-      # サインアップ時にnameとavatarのストロングパラメータを追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, 
+      keys: [
+      :name,
+      :genre,
+      :avatar,
+      :avata_cache,
+      :remove_avatar,
+      :team_name,
+      :pref1,
+      :pref2,
+      :pref3,
+      :carrer,
+      :lesson_comment,
+      :movie_url,
+      :promotion_url,
+      :twitter_url,
+      :instagram_url,
+      :facebook_url])
     end
-      # ログイン後、home/indexに移動する
-    def after_sign_in_path_for(resource)
-      root_path
+
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
     end
+
 end

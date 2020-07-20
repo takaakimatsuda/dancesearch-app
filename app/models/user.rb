5134a902afcc3772811eae3384fe57d756f4f160
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-07-20 14:49:51 +0900

    enumerizeのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,67 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one_attached :avatar
+
+  PREF_NAMES = [:hokkaido,
+                :aomori,
+                :iwate,
+                :miyagi,
+                :akita,
+                :yamagata,
+                :fukushima,
+                :ibaraki,
+                :tochigi,
+                :gunma,
+                :saitama,
+                :chiba,
+                :tokyo,
+                :kanagawa,
+                :niigata,
+                :toyama,
+                :ishikawa,
+                :fukui,
+                :yamanashi,
+                :nagano,
+                :gifu,
+                :shizuoka,
+                :aichi,
+                :mie,
+                :shiga,
+                :kyoto,
+                :osaka,
+                :hyogo,
+                :nara,
+                :wakayama,
+                :tottori,
+                :shimane,
+                :okayama,
+                :hiroshima,
+                :yamaguchi,
+                :tokushima,
+                :kagawa,
+                :ehime,
+                :kochi,
+                :fukuoka,
+                :saga,
+                :nagasaki,
+                :kumamoto,
+                :oita,
+                :miyazaki,
+                :kagoshima,
+                :okinawa]
+  validate :require_unique_pref, on: :update
   validates :name, presence: true #空文字でないこと
+
+  extend Enumerize
+  enumerize :genre, in: [:hiphop, :pop, :break, :waack, :lock, :tut, :jazz, :freestyle]
+  enumerize :pref1, in: PREF_NAMES
+  enumerize :pref2, in: PREF_NAMES
+  enumerize :pref3, in: PREF_NAMES
+
+  def require_unique_pref
+    if pref1 == pref2 || pref1 == pref3 || pref2 == pref3
+       errors.add(:pref1, '地域が重複しています')
+    end 
+  end
 
 end

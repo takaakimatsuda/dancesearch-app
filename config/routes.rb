53b5b7af09c8ef2fa297fd2058b150cc65e11ac7
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-07-23 13:42:05 +0900

    relationshipsコントローラ、ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   resources :users do
+    resource :relationships, only: [:create, :destroy]
+    member do
+      get :follows
+      get :followers
+    end
     resources :promotions, only: [:create, :destroy]
   end
   root 'home#index'

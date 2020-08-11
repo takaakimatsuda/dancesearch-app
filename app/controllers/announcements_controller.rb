8ef575a16a2201ca0dcecb373c3f7863effbe31f
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-08-11 22:34:27 +0900

    アナウンス画面の作成

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,13 +1,27 @@
 class AnnouncementsController < ApplicationController
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: [:show]
 
   def index
+    @announcement = Announcement.new
+    if @announcements.present?
+      @announcements = @user.announcements.page(params[:page]).per(10)
+    end
   end
 
   def show
+    @announcement = Announcement.new
+    @announcements = @user.announcements.all.page(params[:page]).per(10)
+    @scores = @user.scores.all.sum(:point)
   end
 
   def create
+    @announcement = current_user.announcements.build(announcement_params)
+    if @announcement.save
+      flash[:success] = "announcement created!"
+      redirect_to announcements_path
+    else
+      render 'announcements/index'
+    end
   end
 
   def destroy
@@ -15,7 +29,11 @@ class AnnouncementsController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def announcement_params
+      params.require(:announcement).permit(:content)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     resources :promotions, only: [:create, :destroy]
     resources :scores, only: [:create, :destroy]
     resources :lessons, only: [:create, :destroy, :update]
-    resources :announcements, only: [:index, :show, :create, :destroy]
   end
+  resources :announcements, only: [:index, :show, :create, :destroy]
   root 'users#index'
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?

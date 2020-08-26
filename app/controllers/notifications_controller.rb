1190bb199e487dd4bfb37a5cf6599de6fbbda601
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-08-26 20:13:14 +0900

    authenticate_userの追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!, only: %i[index]
   
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)

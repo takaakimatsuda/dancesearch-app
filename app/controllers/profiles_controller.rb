90b8374d64a75d8e597b6f89fd2b9112d03f98d2
Matsushin <matsu1983@gmail.com>
2020-09-04 17:22:55 +0900

    不要なコードを削除

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,9 +3,4 @@ class ProfilesController < ApplicationController
     current_user.avatar.detach
     redirect_to user_path(current_user), notice: '画像を削除しました。'
   end
-
-  private
-    def promotion_params
-      params.require(:promotion).permit(:id, :content)
-    end
 end

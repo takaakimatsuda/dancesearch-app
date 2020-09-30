7b5fbf5ea5f050f07ce22ffb77e2615d01f378e7
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-09-30 11:43:59 +0900

    scoreモデルのテスト実装

--- spec/factories/score.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :score do
-    user
-    point { rand(1..5)}
-  end
-end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -3,15 +3,35 @@ require 'rails_helper'
 RSpec.describe Score, type: :model do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
-
   describe 'スコア登録のバリデーションテスト' do
 
     context 'スコアポイントが正しい場合' do
       it '正常に登録できる' do
-        other_user.writer_scores = user.scores.build(
-          point: '1',
+        score = user.scores.build(
+        point: '1',
         )
-        expect(writer_scores).to be_valid
+        score.writer = other_user
+        score.save
+        expect(score).to be_valid
+      end
+    end
+    
+    context 'スコアポイントを4回入れた場合' do
+      it 'バリデーションがかかる' do
+        for num in 1..3 do
+          score = user.scores.build(
+            point: '1',
+            )
+          score.writer = other_user
+          score.save
+        end
+        score = user.scores.build(
+          point: '1',
+          )
+        score.writer = other_user
+        score.save
+        user.valid?
+        expect(score.errors[:point]).to include('の入力は1日３回までです')
       end
     end
   end

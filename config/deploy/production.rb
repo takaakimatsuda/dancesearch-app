b141a2d682b608fc794fe02bd44409bb8857debe
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-10-13 20:12:49 +0900

    内容の微修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,10 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 # EC2サーバーのIP、EC2サーバーにログインするユーザー名、サーバーのロールを記述
-server '18.180.92.199', user: 'takaaki', roles: %w{app db web} 
+server '18.180.92.199', user: 'ec2-user', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/dancesearch-app_key_rsa' 
+set :ssh_options, keys: '~/.ssh/dancesearch-app.pem' 
 
 
 # role-based syntax

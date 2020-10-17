452ffa7d0c4edc03f338a0963dffc8dda1119f92
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-10-17 17:54:13 +0900

    test3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@
 server '18.180.92.199', user: 'takaaki', roles: %w{app db web} 
 
 #デプロイするサーバーにsshログインする鍵の情報を記述
-set :ssh_options, keys: '~/.ssh/dancesearch-app.pem' 
+set :ssh_options, keys: '~/.ssh/dancesearch-app_key_rsa' 

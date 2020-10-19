248331af153a1d147d444301cc8e0ab0e74cfa1c
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-10-19 22:00:09 +0900

    不要なコードの削除

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,9 +2,4 @@ class Relationship < ApplicationRecord
   belongs_to :following, class_name: "User"
   belongs_to :follower, class_name: "User"
   
-  # ransacker :followers_count do
-  #   query = '(SELECT COUNT(relationships.follower_id) FROM relationships GROUP BY relationships.follower_id ORDER BY count(relationships.follower_id) DESC)'
-  #   Arel.sql(query)
-  # end
-  
 end

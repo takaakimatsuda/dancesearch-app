c5e842cede0b14e8a07d22f4f77dfefbe713a4cd
松田　剛明 <takaaki-matsuda@hotmail.co.jp>
2020-10-23 21:41:48 +0900

    不要なコードの削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_to, '/var/www/dancesearch-app'
 
 # シンボリックリンクをはるファイル。
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/master.key')
-append :linked_files, 'config/master.key'
+
 
 # シンボリックリンクをはるフォルダ。
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
